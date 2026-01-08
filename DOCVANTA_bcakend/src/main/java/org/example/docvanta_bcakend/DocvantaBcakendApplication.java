60652b612f6cac8185c8882466dd919911796054
mohammed elmostaine <mohammedelmostaine@gmail.com>
2026-01-08 11:00:00 +0100

    DOC-230 add main application class and backend configuration files

---
+++ DOCVANTA_bcakend/src/main/java/org/example/docvanta_bcakend/DocvantaBcakendApplication.java
@@ -0,0 +1,13 @@
+package org.example.docvanta_bcakend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocvantaBcakendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocvantaBcakendApplication.class, args);
+    }
+
+}
